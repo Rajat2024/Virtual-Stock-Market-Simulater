4f4e2138287f82da00745dd5c2776dcdec412d3e
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-07-15 20:36:20 +0530

    fetchuser.js added

---
+++ server/middleware/fetchuser.js
@@ -0,0 +1,30 @@
+var jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+const secret = process.env.SECRET_KEY;
+
+const fetchuser = (req, res, next) => {
+  // get the user from jwt token and append user_id to req object
+  const token = req.header("auth-token");
+
+  if (!token) {
+    return res.status(401).send({ error: "Please use a valid token" });
+  }
+  try {
+    // here data is the payload(user_id) from which token is formed
+    // here we take token from request header and then we verify with secret_key
+    // and take out the payload(user_id) from token using secret_key
+    // add user_id in request body
+
+    const data = jwt.verify(token, secret);
+
+    // console.log("data", data);
+    req.user = data.user;
+
+    next(); // then process the next step
+  } catch (error) {
+    res.status(401).send({ error: "Please use a valid token" });
+  }
+};
+
+module.exports = fetchuser;
