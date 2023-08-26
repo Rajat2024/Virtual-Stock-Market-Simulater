52a640e02e71c7b99f819bdfe7d42e34f9773b87
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-08-27 01:03:09 +0530

    Delete frontend directory

--- frontend/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Login } from "./Authentication/Login";
-export { default as Register } from "./Authentication/Register";
-export { default as PageTemplate } from "./Template/PageTemplate";
-export { default as NotFound } from "./NotFound/NotFound";
