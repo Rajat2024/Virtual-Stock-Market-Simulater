7b58682513d56727b59be581b6651d333336bbec
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-12-18 10:36:52 +0530

    added API KEYS

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -3,7 +3,12 @@ const Axios = require("axios");
 exports.getNewsData = async (req, res) => {
 
   try {
-
+    // these are free API_KEY
+    // pub_34966bd750465fa9067e371e2ff8e61c990a9
+    // pub_34967b44b0d25a5ec1096c1b2135672c1004f
+    // pub_34969f0cb245aa914cfc7aee2e1232ce1a146
+    // pub_349701ebbe1cefd9915e5af490160cb3dd523
+    
     console.log("API is Called");
     const token = process.env.NEWS_API_KEY;
     const url2 = `https://newsapi.org/v2/everything?q=business&sortBy=publishedAt&apiKey=${token}&language=en&pageSize=6&page=${req.params.page}`;
